60ba7c5f6a1209fc20b6a868aba823c05d501779
Denis G <DenisGrigoriev04@yandex.ru>
2023-11-22 20:40:13 +0300

    Реализованна регестрация С хешированием пароля через crypto::hash::Blake2b128

--- GameBackendServer/mongo_service.cpp
+++ GameBackendServer/mongo_service.cpp
@@ -5,11 +5,190 @@
 #include <userver/server/handlers/http_handler_base.hpp>
 #include <userver/storages/mongo/component.hpp>
 #include <userver/utils/daemon_run.hpp>
+#include <userver/formats/json.hpp>
+#include <userver/crypto/hash.hpp>
+#include <string>
 
 #include <userver/utest/using_namespace_userver.hpp>
 
+namespace GameBackendServer {
+
+
+class UserRegistrationHandler final : public server::handlers::HttpHandlerBase {
+public:
+  static constexpr std::string_view kName = "handler-user-registration";
+  storages::mongo::PoolPtr pool_;
+
+  UserRegistrationHandler(const components::ComponentConfig& config,
+                          const components::ComponentContext& context)
+    : HttpHandlerBase(config, context),
+      pool_(context.FindComponent<components::Mongo>("mongo-tr").GetPool()) {}
+
+  std::string HandleRequestThrow(
+    const server::http::HttpRequest& request,
+    server::request::RequestContext&) const override {
+      formats::json::Value json = formats::json::FromString(request.RequestBody());
+      const std::string username = json["username"].As<std::string>();
+      const std::string password = json["password"].As<std::string>();
+      auto users = pool_->GetCollection("users");
+      using formats::bson::MakeDoc;
+      size_t count_user = users.Count(MakeDoc("username", username));
+      formats::json::ValueBuilder vb;
+      if (count_user != 0) {
+        vb["detail"] = "User with this username already exists";
+        request.SetResponseStatus(server::http::HttpStatus::kConflict);
+      } else {
+        const std::string hashed_password = crypto::hash::Blake2b128(password);
+        users.InsertOne(MakeDoc("username", username,"hashed_password", hashed_password));
+        formats::json::ValueBuilder vb;
+        vb["username"] = username;
+        request.SetResponseStatus(server::http::HttpStatus::kCreated);     
+      };
+      return ToString(vb.ExtractValue());
+    };
+};
+
+
+/*
+
+class UserLoginHandler final : public server::handlers::HttpHandlerBase {
+public:
+  static constexpr std::string_view kName = "handler-user-login";
+  storages::mongo::PoolPtr pool_;
+
+  UserLoginHandler(const components::ComponentConfig& config,
+                  const components::ComponentContext& context)
+    : HttpHandlerBase(config, context),
+      pool_(context.FindComponent<components::Mongo>("mongo-tr").GetPool()) {}
+
+  std::string HandleRequestThrow(
+    const server::http::HttpRequest& request,
+    server::request::RequestContext&) const override {
+      formats::json::Value json = formats::json::FromString(request.RequestBody());
+      const std::string username = json["username"].As<std::string>();
+      const std::string password = json["password"].As<std::string>();
+      auto users = pool_->GetCollection("users");
+      using formats::bson::MakeDoc;
+      size_t count_user = users.Find(MakeDoc("username", username));
+      formats::json::ValueBuilder vb;
+      if (count_user == 0) {
+        vb["detail"] = "Incorrect username or password";
+        request.SetResponseStatus(server::http::HttpStatus::kUnauthorized);
+      } else {
+        auto user = users.Find(MakeDoc("username", username));
+        const std::string hashed_true_password = crypto::hash::Blake2b128(password);
+        const std::string hashed_user_password = crypto::hash::Blake2b128(password);
+        if (hashed_true_password != hashed_user_password) {
+          vb["detail"] = "Incorrect username or password";
+          request.SetResponseStatus(server::http::HttpStatus::kUnauthorized)
+        } else {
+            //TODO
+        };
+
+        formats::json::ValueBuilder vb;
+        vb["access_token"] = access_token;
+        vb["token_type"] = token_type;
+        request.SetResponseStatus(server::http::HttpStatus::kOk);     
+      };
+      return ToString(vb.ExtractValue());
+    };
+};
+
+
+class UserMeHandler final : public server::handlers::HttpHandlerBase {
+public:
+  static constexpr std::string_view kName = "handler-user-me";
+  storages::mongo::PoolPtr pool_;
+
+  UserMeHandler(const components::ComponentConfig& config,
+                  const components::ComponentContext& context)
+    : HttpHandlerBase(config, context),
+      pool_(context.FindComponent<components::Mongo>("mongo-tr").GetPool()) {}
+
+  std::string HandleRequestThrow(
+    const server::http::HttpRequest& request,
+    server::request::RequestContext&) const override {};
+  private:
+
+};
+
+
+class UserOtherHandler final : public server::handlers::HttpHandlerBase {
+public:
+  static constexpr std::string_view kName = "handler-user-other";
+  storages::mongo::PoolPtr pool_;
+
+  UserOtherHandler(const components::ComponentConfig& config,
+                  const components::ComponentContext& context)
+    : HttpHandlerBase(config, context),
+      pool_(context.FindComponent<components::Mongo>("mongo-tr").GetPool()) {}
+
+  std::string HandleRequestThrow(
+    const server::http::HttpRequest& request,
+    server::request::RequestContext&) const override {
+      formats::json::Value json = formats::json::FromString(request.RequestBody());
+      const std::string username = json["username"].As<std::string>();
+      const std::string password = json["password"].As<std::string>();
+      auto users = pool_->GetCollection("users");
+      using formats::bson::MakeDoc;
+      size_t count_user = users.Find(MakeDoc("username", username));
+      formats::json::ValueBuilder vb;
+      if (count_user == 0) {
+        vb["detail"] = "Incorrect username or password";
+        request.SetResponseStatus(server::http::HttpStatus::kUnauthorized);
+      } else {
+        auto user = users.Find(MakeDoc("username", username));
+        const std::string hashed_true_password = crypto::hash::Blake2b128(password);
+        const std::string hashed_user_password = crypto::hash::Blake2b128(password);
+        if (hashed_true_password != hashed_user_password) {
+          vb["detail"] = "Incorrect username or password";
+          request.SetResponseStatus(server::http::HttpStatus::kUnauthorized)
+        } else {
+            //TODO
+        };
+
+        formats::json::ValueBuilder vb;
+        vb["access_token"] = access_token;
+        vb["token_type"] = token_type;
+        request.SetResponseStatus(server::http::HttpStatus::kOk);     
+      };
+      return ToString(vb.ExtractValue());
+    };
+};
+
+
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 /// [Mongo service sample - component]
 namespace samples::mongodb {
+
+
 
 class Translations final : public server::handlers::HttpHandlerBase {
  public:
@@ -97,13 +276,12 @@ std::string Translations::ReturnDiff(
 /// [Mongo service sample - ReturnDiff]
 
 }  // namespace samples::mongodb
+*/
 
-/// [Mongo service sample - main]
 int main(int argc, char* argv[]) {
   const auto component_list = components::MinimalServerComponentList()
                                   .Append<clients::dns::Component>()
                                   .Append<components::Mongo>("mongo-tr")
-                                  .Append<samples::mongodb::Translations>();
+                                  .Append<GameBackendServer::UserRegistrationHandler>();
   return utils::DaemonMain(argc, argv, component_list);
 }
-/// [Mongo service sample - main]
